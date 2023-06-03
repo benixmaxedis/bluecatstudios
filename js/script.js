3e79ab9f172939acd3624a8b1f4df6ebf6c5888a
Ben Wales <benwales@bigpond.com>
2023-06-03 12:27:23 +1000

    updated particle system

--- js/script.js
+++ js/script.js
@@ -1,90 +1,83 @@
 const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-const particleArray = [];
-let hue = 0;
+const ctx = canvas.getContext("2d");
 
-window.addEventListener("resize", function () {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-});
-
-const mouse = {
-  x: undefined,
-  y: undefined,
-};
-
-window.addEventListener("click", function (event) {
-  mouse.x = event.x;
-  mouse.y = event.y;
-  for (let i = 0; i < 10; i++) {
-    particleArray.push(new Particle());
-  }
-});
-
-window.addEventListener("mousemove", function (event) {
-  mouse.x = event.x;
-  mouse.y = event.y;
-  for (let i = 0; i < 2; i++) {
-    particleArray.push(new Particle());
-  }
-});
+const particlesArray = [];
+const colors = [
+  "#f44336",
+  "#e91e63",
+  "#9c27b0",
+  "#673ab7",
+  "#3f51b5",
+  "#2196f3",
+  "#03a9f4",
+  "#00bcd4",
+  "#009688",
+  "#4caf50",
+  "#8bc34a",
+  "#cddc39",
+  "#ffeb3b",
+  "#ffc107",
+  "#ff9800",
+  "#ff5722",
+];
 
 class Particle {
-  constructor() {
-    this.x = mouse.x;
-    this.y = mouse.y;
-    this.size = Math.random() * 15 + 1;
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = Math.random() * 5 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
-    this.color = "hsl(" + hue + ",100%,50%)";
+    this.color = colors[Math.floor(Math.random() * colors.length)];
   }
+
   update() {
+    this.size -= 0.1;
+    if (this.size < 0) this.size = 0;
     this.x += this.speedX;
     this.y += this.speedY;
-    if (this.size > 2) this.size -= 0.1;
   }
+
   draw() {
     ctx.fillStyle = this.color;
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 0.5;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.closePath();
     ctx.fill();
+    ctx.stroke();
   }
 }
 
-function handleParticles() {
-  for (let i = 0; i < particleArray.length; i++) {
-    particleArray[i].update();
-    particleArray[i].draw();
+function createParticles(e) {
+  const xPos = e.x;
+  const yPos = e.y;
 
-    for (let j = i; j < particleArray.length; j++) {
-      const dx = particleArray[i].x - particleArray[j].x;
-      const dy = particleArray[i].y - particleArray[j].y;
-      const distance = Math.sqrt(dx * dx + dy * dy);
-      if (distance < 100) {
-        ctx.beginPath();
-        ctx.strokeStyle = particleArray[i].color;
-        ctx.lineWidth = particleArray[i].size / 10;
-        ctx.moveTo(particleArray[i].x, particleArray[i].y);
-        ctx.lineTo(particleArray[j].x, particleArray[j].y);
-        ctx.stroke();
-      }
-    }
+  for (let i = 0; i < 5; i++) {
+    particlesArray.push(new Particle(xPos, yPos));
+  }
+}
 
-    if (particleArray[i].size <= 0.3) {
-      particleArray.splice(i, 1);
+function animateParticles() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (let i = 0; i < particlesArray.length; i++) {
+    particlesArray[i].update();
+    particlesArray[i].draw();
+
+    if (particlesArray[i].size <= 0.1) {
+      particlesArray.splice(i, 1);
       i--;
     }
   }
+
+  requestAnimationFrame(animateParticles);
 }
 
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  //   ctx.fillStyle = "rgba(0,0,0,0.02)";
-  //   ctx.fillRect(0, 0, canvas.width, canvas.height);
-  handleParticles();
-  hue += 0.5;
-  requestAnimationFrame(animate);
-}
-animate();
+canvas.addEventListener("click", createParticles);
+
+canvas.addEventListener("mousemove", createParticles);
+
+animateParticles();
